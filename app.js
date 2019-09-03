c3a06550448330775be1e9fdff4990183c238e97
Kei Hendrikz <48445517+keincoffee@users.noreply.github.com>
2019-09-03 13:32:04 -0500

    added requireHTTPS Function

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ let options = {
 const app = express();
 const bodyParser = require('body-parser');
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
+app.use(requireHTTPS);
 app.use(bodyParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
